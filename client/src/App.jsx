58892c6e1e0772d05ccaae501e933e26e3e8f0aa
decipher07 <social.deej@gmail.com>
2022-12-11 14:28:25 +0530

    💄 feat: adding the payment system

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
+import Payment from './pages/Payment'
 
 function App() {
   return (
     <Routes>
       <Route index element={<Navigate to="login" />}/>
       <Route path='login' element={<Login />}/>
+      <Route path='payment' element={<Payment />}/>
     </Routes>
   )
 }
---
+++ client/src/pages/Payment.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import "./Payment.css"
+import { batchIdAndTimings, monthIdAndMonth, backendUrl } from '../utils/constants'
+
+function Payment() {
+
+    const [pendingTransactions, setPendingTransactions] = useState(null);
+
+    useEffect(() => {
+        // Fetch data from backend
+        async function fetchData() {
+            const userId = localStorage.getItem("userId");
+            const response = await axios.get(`${backendUrl}/payment/pendingtranscations/${userId}`);
+            setPendingTransactions(response.data.data);
+        }
+
+        fetchData();
+
+    }, []);
+
+    // Making payments
+    const makePayment = async (e, monthId) => {
+        try {
+            const body = { "month": monthId, "userId": localStorage.getItem("userId") };
+            alert(`You're making a payment for the month of ${monthIdAndMonth.get(monthId)} or Rs 500/-`)
+            await axios.post(`${backendUrl}/payment/makepayment`, body);
+        } catch (err) {
+            console.log(err);
+            alert(err.response.data.message);
+        }
+    }
+
+    return (
+        <div className="main">
+            {/* Payment Frame */}
+            <div className="paymentFrame">
+                
+                {/* Heading for Payment */}
+                <div className='headers'>
+                    <h1>Here are your pending transactions</h1>
+                </div>
+                
+                {/* Defining Tables */}
+                <div className="tableHeaders">
+                    <div className="monthColumn">
+                        <h5>Month</h5>
+                    </div>
+                    <h5>Batch Timings</h5>
+                </div>
+
+                {/* Populating the response from pending transactions */}
+                {
+                    pendingTransactions ? pendingTransactions.map((item) => {
+                        return (
+                            <div className="paymentListing">
+                                <div className='monthDivision'>
+                                    <h5>{monthIdAndMonth.get(item.month)}</h5>
+                                </div>
+                                <h5>{item.batchTiming.start_time}</h5>
+                                <h5>{item.batchTiming.end_time}</h5>
+                                <h5>Rs {item.amount}/-</h5>
+                                <button className="btn btn-dark submit" value="Signup" onClick={(e) => {
+                                        makePayment(e, item.month)
+                                        e.target.disabled = true
+                                    }}>
+                                    Pay Now
+                                </button>
+                            </div>)
+                    }) : null
+                }
+
+                {/* Footer */}
+                <div className="footerFrame">
+                    <a href="http://localhost:5173/login">
+                        <input type="button" className="btn btn-dark submit" value="Back To Signup" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Payment
