7130925f0e1e5230d446b29f8dc40f7acd1db1d6
decipher07 <social.deej@gmail.com>
2022-12-11 13:23:44 +0530

    👥 constants: utils for month and batch

---
+++ client/src/utils/constants.js
@@ -0,0 +1,24 @@
+
+/** Setting BatchId and Hashmaps */
+const batchIdAndTimings = new Map();
+batchIdAndTimings.set(1, "6.00 to 7.00");
+batchIdAndTimings.set(2, "7.00 to 8.00");
+batchIdAndTimings.set(3, "8.00 to 9.00");
+batchIdAndTimings.set(4, "17.00 to 18.00");
+
+/** Setting monthId and Hashmaps */
+const monthIdAndMonth = new Map();
+monthIdAndMonth.set(1, "January");
+monthIdAndMonth.set(2, "February");
+monthIdAndMonth.set(3, "March");
+monthIdAndMonth.set(4, "April");
+monthIdAndMonth.set(5, "May");
+monthIdAndMonth.set(6, "June");
+monthIdAndMonth.set(7, "July");
+monthIdAndMonth.set(8, "August");
+monthIdAndMonth.set(9, "September");
+monthIdAndMonth.set(10, "October");
+monthIdAndMonth.set(11, "November");
+monthIdAndMonth.set(12, "December");
+
+export { batchIdAndTimings, monthIdAndMonth };
