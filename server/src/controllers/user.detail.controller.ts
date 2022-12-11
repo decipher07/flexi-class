29dc78d1bda47961c2ae793055e50edb7b5cdc6a
decipher07 <social.deej@gmail.com>
2022-12-11 14:28:07 +0530

    🚑 fix: adding the userid in response

--- server/src/controllers/user.detail.controller.ts
+++ server/src/controllers/user.detail.controller.ts
@@ -70,7 +70,7 @@ const createUserController = async ( req: Request, res: Response, next: NextFunc
             return res.status(201).json({"success": true, "data": { "userId" : userIdForTheDocument }, "message": null});    
         }
 
-        return res.status(403).json({"success": false, "data": null, "message": "You've already booked a similar transaction. Please complete the transaction"});
+        return res.status(403).json({"success": false, "data": { "userId" : userIdForTheDocument }, "message": "You've already booked a similar transaction. Please complete the transaction"});
 
     } catch ( err: any ) {
         Logging.error(err.message);
